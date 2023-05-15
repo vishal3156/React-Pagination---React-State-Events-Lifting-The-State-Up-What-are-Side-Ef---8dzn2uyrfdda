4ef9c72f217dd2d37080cb915bdd9a0fb40edf31
Vishal Suman <119152809+vishal3156@users.noreply.github.com>
2023-05-15 23:39:47 +0530

    Update fetchPosts.js

--- src/api/fetchPosts.js
+++ src/api/fetchPosts.js
@@ -1,5 +1,6 @@
 const fetchPosts = async (page, limit) => {
-    // optionally write fetching logic here or somewhere else
-}
+  let url = `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`;
+  return fetch(url);
+};
 
-export { fetchPosts }
+export { fetchPosts };
